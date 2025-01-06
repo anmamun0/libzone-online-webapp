efe368a4233841c3169494613dd4eeea4d7da985
anmamun0 <anmamun0@gmail.com>
2025-01-07 02:08:01 +0600

    postgreSQL database added

--- libzone/books/models.py
+++ libzone/books/models.py
@@ -13,7 +13,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     isbn = models.CharField(max_length=13, unique=True)  # Unique ISBN number
-    category = models.ManyToManyField(Category,   blank=True,related_name='books') 
+    category = models.ManyToManyField(Category, blank=True,related_name='books') 
     publication_date = models.DateField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField() 
